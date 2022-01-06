c182686ba91d84200a03590767a726cceb915eaa
julia-prestes <contatojuliaprestes@gmail.com>
2022-01-06 14:05:31 -0300

    início do projeto

---
+++ app/models/negociacao.ts
@@ -0,0 +1,18 @@
+export class Negociacao {
+
+    constructor(
+        private _data: Date,
+        public readonly _quantidade: number,
+        public readonly _valor: number
+    ) {}
+
+
+    get volume() : number {
+        return this._quantidade * this._valor;
+    }
+
+    get data(): Date {
+        const data = new Date(this._data.getTime());
+        return this._data;
+    }
+}
